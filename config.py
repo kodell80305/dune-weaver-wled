6b00fe744b4d93c322b96b116e251f8969119526
Kevin Odell <kodell80305@gmail.com>
2025-03-14 13:43:21 -0600

    Change default led for testing

--- config.py
+++ config.py
@@ -19,7 +19,7 @@
 simulate = True
 #You'll need to configure the number of pixels and the type of strip you have.   Also if you want to have the first SEGMENT_0_START pixels be used for undertable
 #lighting, set SEGMENT_0_START.  Otherwise make it 0.  These pixels will always be white, but will respond to brightness/power on or off
-LED_COUNT = 87
+LED_COUNT = 200
 SEGMENT_0_START = 11
 
 
